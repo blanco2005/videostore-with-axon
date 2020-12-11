9a9dd80580c7e8d3120207623bb17ee5a3890184
fbianchi <francesco.bianchi@lastminute.com>
2020-12-11 15:13:16 +0100

    fixed module dep

--- webapp/src/main/java/com/fb/videostore/adapter/AxonMovieService.java
+++ webapp/src/main/java/com/fb/videostore/adapter/AxonMovieService.java
@@ -1,14 +1,20 @@
 package com.fb.videostore.adapter;
 
+import com.fb.videostore.RegisterMovieCommand;
 import com.fb.videostore.service.MovieService;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class AxonMovieService implements MovieService {
 
+    @Autowired
+    private CommandGateway commandGateway;
+
     @Override
     public void register(String serialNumber, String title) {
-
+        commandGateway.send(new RegisterMovieCommand(serialNumber, title));
     }
 
     @Override
